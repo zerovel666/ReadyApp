e73f1021ac977c8f98e2dd851f9b2d3a8d48845c
zerovel666 <zerovel060606@gmail.com>
2025-07-07 00:10:27 +0500

    refactor

--- app/Http/Helpers/Response.php
+++ app/Http/Helpers/Response.php
@@ -26,38 +26,6 @@ class Response
         ]);
 
         return json_decode($response->getBody(), true);
-        // try {
-        //     $client = new Client();
-        //     $url = $domen . self::getUrlPath($request);
-        //     $response = $client->get($url, [
-        //         'headers' => $headers ?? [],
-        //         'query' => $query ?? [],
-        //     ]);
-
-        //     return json_decode($response->getBody(), true);
-        // } catch (RequestException $e) {
-        //     $response = $e->getResponse();
-        //     $clientRequest = $e->getRequest();
-        //     $bodyError = json_decode($response->getBody());
-        //     if (
-        //         isset($bodyError->message) &&
-        //         isset($bodyError->line) &&
-        //         $clientRequest->getUri()
-        //     ) {
-        //         $body = [
-        //             "message" => $bodyError->message,
-        //             "line" => $bodyError->line,
-        //             "route" => $request->path(),
-        //         ];
-        //     } else {
-        //         $body = [
-        //             "message" => "Ошибка при вызове AgentService",
-        //             "route" => $request->path(),
-        //         ];
-        //     }
-
-        //     return self::response($body, false, $response->getStatusCode());
-        // }
     }
 
     public static function getUrlPath(Request $request)
