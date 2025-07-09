e298285715f030512c8660007257ed69631833c0
zerovel666 <zerovel060606@gmail.com>
2025-07-09 08:48:29 +0500

    .

---
+++ database/migrations/2025_07_09_034155_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("email");
+            $table->text("password");
+            $table->string("first_name");
+            $table->string("parent_name");
+            $table->string("last_name");
+            $table->string('full_name');
+            $table->foreignId("country_id")->constrained("countries")->onDelete("cascade");
+            $table->string("uniq_id_people")->unique();
+            $table->foreignId("partner_id")->nullable()->constrained("partners")->onDelete("cascade");
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
