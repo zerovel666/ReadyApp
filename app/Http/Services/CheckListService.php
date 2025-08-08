9a9c05c29ad43fe1599b7fe6c1cd977ce75727d9
zerovel666 <zerovel060606@gmail.com>
2025-08-08 12:43:36 +0500

    fix

--- app/Http/Services/CheckListService.php
+++ app/Http/Services/CheckListService.php
@@ -6,7 +6,20 @@ use App\Http\Repository\CheckListRepository;
 
 class CheckListService extends BaseService
 {
-    public function __construct(CheckListRepository $checkListRepository) {
+    public function __construct(CheckListRepository $checkListRepository)
+    {
         parent::__construct($checkListRepository);
+    }
+
+    public function updateById($id, $attributes)
+    {
+        $checkListItem = $this->repository->find($id);
+
+        $task = $checkListItem->task;
+        if ($task->status->constant !== "IN_WORK") {
+            throw new \Exception("You cannot edit the form while the task is not in progress.", 422);
+        }
+
+        return $this->repository->updateById($id, $attributes);
     }
 }
--- app/Http/Services/TaskService.php
+++ app/Http/Services/TaskService.php
@@ -18,18 +18,4 @@ class TaskService extends BaseService
         return $this->repository->getByColumn("agent_id",$agent_id);
     }
 
-    public function updateById($id, $attributes)
-    {
-        $checkListItem = $this->repository->find($id);
-        if (!$checkListItem){
-            throw new \Exception("Object form item task with id $id not found",404);
-        }
-
-        $task = $checkListItem->task;
-        if ($task->status->constant !== "IN_WORK"){
-            throw new \Exception("You cannot edit the form while the task is not in progress.",422);
-        }
-
-        return $this->repository->updateById($id,$attributes);
-    }
 }
