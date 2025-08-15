395d284abed4543d0ba588afec8add8ebc839f4d
zerovel666 <zerovel060606@gmail.com>
2025-08-15 16:17:50 +0500

    add check role

--- app/Models/Dicti.php
+++ app/Models/Dicti.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Auth;
 
 class Dicti extends Model
 {
@@ -26,7 +28,16 @@ class Dicti extends Model
 
     protected $table = 'dictis';
 
-        public function children(): HasMany
+    protected static function booted()
+    {
+        static::addGlobalScope('available', function (Builder $builder) {
+            $user = Auth::user();
+            if (!$user || !$user->roles()->where("slug", "admin")->exists()) {
+                $builder->where('active', true);
+            }
+        });
+    }
+    public function children(): HasMany
     {
         return $this->hasMany(Dicti::class, "parent_id", "id");
     }
